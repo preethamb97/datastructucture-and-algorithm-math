47fb1972d555632f28fc388307fec7c4d120d401
Preetham B <preethamb97@gmail.com>
2021-07-21 23:15:43 +0530

    priority queue added

--- JSIMPLEMENTS/queue.js
+++ JSIMPLEMENTS/queue.js
@@ -3,7 +3,7 @@
 
 // size, enqueue, dequeue, front, print, isempty
 const basicQueues = () => {
-  var myQueue = function () {
+  let myQueue = function () {
     this.collection = [];
 
     this.print = function () {
@@ -43,8 +43,70 @@ const basicQueues = () => {
   console.log('=====front', basicQueue.front())
 }
 
+const priorityQueue = () => {
+  let myQueue = function () {
+    this.collection = [];
+
+    this.print = function () {
+      return this.collection;
+    }
+
+    this.size = function () {
+      return this.collection.length;
+    }
+
+    this.enqueue = function (value) {
+      let isAdded = false;
+      if (this.collection.length == 0) {
+        this.collection.push(value);
+        return this.collection[0];
+      }
+
+      for (let i = 0; i < this.collection.length; i++) {
+        if (this.collection[i][1] > value[1]) {
+          this.collection.splice(i, 0, value);
+          isAdded = true;
+          break;
+        }
+      }
+      if (isAdded == false) {
+        this.collection.push(value);
+      }
+      return value;
+
+    }
+
+    this.dequeue = function () {
+      this.collection.shift();
+      return this.collection;
+    }
+
+    this.isEmpty = function () {
+      return (this.collection.length == 0);
+    }
+  }
+
+  let myPriorityQueue = new myQueue();
+  console.log('--------------priority queues-------------------');
+  myPriorityQueue.enqueue(["rank 10", 10]);
+  console.log('print=======', myPriorityQueue.print());
+  console.log('length=======', myPriorityQueue.size());
+  myPriorityQueue.enqueue(["rank 5", 5]);
+  myPriorityQueue.enqueue(["rank 2", 2]);
+  myPriorityQueue.enqueue(["rank 7", 7]);
+  myPriorityQueue.enqueue(["rank 24", 24]);
+  myPriorityQueue.enqueue(["rank 15", 15]);
+  console.log('print=======', myPriorityQueue.print());
+  console.log('length=======', myPriorityQueue.size());
+  myPriorityQueue.dequeue();
+  myPriorityQueue.dequeue();
+  console.log('print=======', myPriorityQueue.print());
+  console.log('length=======', myPriorityQueue.size());
+};
+
 const mainFunction = () => {
   basicQueues();
+  priorityQueue();
 }
 
 mainFunction();
