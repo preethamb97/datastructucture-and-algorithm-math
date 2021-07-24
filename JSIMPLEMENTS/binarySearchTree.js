fd42ad1d00391eefc5f190bca8a4a1e28aa2664f
Preetham B <preethamb97@gmail.com>
2021-07-24 23:52:38 +0530

    bst min, max find

--- JSIMPLEMENTS/binarySearchTree.js
+++ JSIMPLEMENTS/binarySearchTree.js
@@ -43,6 +43,22 @@ class BST {
     }
   }
 
+  findMin() {
+    let current = this.root;
+    while(current.left != null) {
+      current = current.left;
+    }
+    return current.data;
+  }
+
+  findMax() {
+    let current = this.root;
+    while(current.right != null) {
+      current = current.right;
+    }
+    return current.data;
+  }
+
 
 }
 
