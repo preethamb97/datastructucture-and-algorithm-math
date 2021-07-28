8f4bd1bb4ce40d6a483ec37a607fd5508c8c6587
Preetham B <preethamb97@gmail.com>
2021-07-28 22:57:25 +0530

    find min, fin max, is present in bst

--- JSIMPLEMENTS/binarySearchTree.js
+++ JSIMPLEMENTS/binarySearchTree.js
@@ -17,49 +17,81 @@ class BST {
       this.root = new Node(data);
       return data;
     } else {
-      const searchTree = function (node) {
+      //
+      function recursiveTree(node) {
+        // console.log(node.data,'====', data)
         if (data < node.data) {
           if (node.left == null) {
             node.left = new Node(data);
             return data;
-          } else if (node.left != null) {
-            return searchTree(node.left);
+          } else {
+            return recursiveTree(node.left);
           }
-
         } else if (data > node.data) {
           if (node.right == null) {
             node.right = new Node(data);
             return data;
-          } else if (node.right != null) {
-            return searchTree(node.right);
+          } else {
+            return recursiveTree(node.right);
           }
-
         } else {
           return null;
         }
       }
-
-      return searchTree(node);
+      return recursiveTree(node);
     }
-  }
-
-  findMin() {
-    let current = this.root;
-    while(current.left != null) {
-      current = current.left;
-    }
-    return current.data;
   }
 
   findMax() {
-    let current = this.root;
-    while(current.right != null) {
-      current = current.right;
+    function recursiveCall(node) {
+      if (node.data == null) {
+        return null;
+      } else if (node.right == null) {
+        return node.data;
+      } else {
+        return recursiveCall(node.right);
+      }
     }
-    return current.data;
+    return recursiveCall(this.root);
   }
 
+  findMin() {
+    function recursiveCall(node) {
+      if (node.data == null) {
+        return null;
+      } else if (node.left == null) {
+        return node.data;
+      } else {
+        return recursiveCall(node.left);
+      }
+    }
+    return recursiveCall(this.root);
+  }
 
+  isPresent(lookUp) {
+    function recursiveCall(node) {
+      if (node.data == null) {
+        return false;
+      } else if (node.data == lookUp) {
+        return true;
+      } else if (lookUp < node.data) {
+        if (node.left == null) {
+          return false;
+        } else {
+          return recursiveCall(node.left);
+        }
+      } else if (lookUp > node.data) {
+        if (node.right == null) {
+          return false;
+        } else {
+          return recursiveCall(node.right);
+        }
+      } else {
+        return false;
+      }
+    }
+    return recursiveCall(this.root);
+  }
 }
 
 let myBST = new BST();
@@ -67,11 +99,14 @@ console.log(myBST.add(10));
 console.log(myBST.add(2));
 console.log(myBST.add(11));
 console.log(myBST.add(22));
-console.log(myBST.add(99));
+console.log(myBST.add(1));
 console.log(myBST.add(33));
 console.log(myBST.add(14));
 console.log(myBST.add(55));
 console.log(myBST.add(12));
 // myBST.remove(7);
-// console.log(myBST.findMax());
-// console.log(myBST.findMax());
+console.log('---------------MAX', myBST.findMax());
+console.log('---------------MIN', myBST.findMin());
+console.log('---------------isPresent', myBST.isPresent(1111));
+console.log('---------------isPresent', myBST.isPresent(1));
+console.log('---------------isPresent', myBST.isPresent(55));
